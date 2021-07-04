f8a492906e635154571da3ab6a6b7ae5d4e53038
guiedington <guilhermeedington@gmail.com>
2021-07-04 09:47:09 -0300

    pigs file

---
+++ pigs.py
@@ -0,0 +1,47 @@
+import pigpio
+import time
+
+pi = pigpio.pi()
+bright = 255
+
+GREEN = 17
+RED = 27
+BLUE = 22
+STEP = 1
+
+def updateColor(color, step):
+    color += step
+    if color > 255:
+        return 255
+    if color < 0:
+        return 0
+    return color
+
+def setLights(pin, brightness):
+    realBrightness = int(int(brightness) * (float(bright)/255.0))
+    pi.set_PWM_dutycycle(pin, realBrightness)
+
+def fading():
+    r = 255
+    b = 0
+    g = 0
+    while true:
+        if r == 255 and b == 0 and g < 255:
+            g = updateColor(g,STEP)
+            setLights(GREEN,g)
+        elif g == 255 and b == 0 and r > 0:
+            r = updateColor(r,-STEP)
+            setLights(RED,r)
+        elif r == 0 and g == 255 and b < 255:
+            b = updateColor(b, STEP)
+            setLights(BLUE,b)
+        elif r == 0 and g > 0 and b == 255:
+            g = updateColor(g, -STEP)
+            setLights(GREEN,g)
+        elif r < 255 and g == 0 and b == 255:
+            r = updateColor(r,STEP)
+            setLights(RED,r)
+        elif r == 255 and g == 0 and b > 0:
+            b = setLights(b,-STEP)
+            setLights(BLUE,b)
+    time.sleep(1)
